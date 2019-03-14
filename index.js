68c9d5ec367447dc6f9da21770a229995cd2ae8f
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-03-14 14:43:46 -0700

    Fix routing

--- index.js
+++ index.js
@@ -4,19 +4,13 @@ const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
 
+const { targets, port } = require('./config');
+
 const app = express();
-const port = 80;
 
 // app.use(morgan('dev'));
 
-const targets = {
-  'music-player': 'example.com/hello',
-  comments: 'example.com',
-  description: 'example.com',
-  sidebar: 'http://localhost:8081',
-};
-
-app.use('/api/music-player/*', proxy({ target: targets['music-player'], changeOrigin: true }));
+app.use('/api/music-player/*', proxy({ target: targets.musicplayer, changeOrigin: true }));
 app.use('/api/comments/*', proxy({ target: targets.comments, changeOrigin: true }));
 app.use('/api/description/*', proxy({ target: targets.description, changeOrigin: true }));
 app.use('/api/sidebar/*', proxy({ target: targets.sidebar, changeOrigin: true }));
@@ -24,9 +18,15 @@ app.use('/graphql', proxy({ target: targets.sidebar, changeOrigin: true }));
 
 
 app.use('/scripts', express.static(path.resolve(__dirname, 'node_modules')));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/song/:songId', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public')));
 
+app.get('/song/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.redirect(301, '/song/1');
+});
 
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
